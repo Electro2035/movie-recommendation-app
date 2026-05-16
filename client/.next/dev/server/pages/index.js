c11dc5c71db26323d11e756c5b47130850bff6a3
CaesarMaulana19 <114760890+Electro2035@users.noreply.github.com>
2026-05-17 00:34:10 +0700

    Delete client/.next/dev directory

--- client/.next/dev/server/pages/index.js
+++
@@ -1,9 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/index.js")
-R.c("server/chunks/ssr/[root-of-the-server]__0sf2dg4._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__0._82xq._.js")
-R.c("server/chunks/ssr/node_modules_129w7gn._.js")
-R.c("server/chunks/ssr/[externals]__0.o~_~l._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__0a5_g5l._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__0gfs4gp._.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/src/pages/index.js [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/src/pages/_app.js [ssr] (ecmascript)\" } [ssr] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/src/pages/index.js [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/src/pages/_app.js [ssr] (ecmascript)\" } [ssr] (ecmascript)").exports
--- client/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"eZRImbBCyUmXHeFptWFSOFLxpD4moBywlLCpbaD1aJ8=\"\n}"
